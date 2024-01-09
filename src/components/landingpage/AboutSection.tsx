af04f66056b1297b24072a2d54f02f1e5f19e66e
Mac <mac@Macs-MacBook-Pro.local>
2024-01-09 09:51:33 +0530

    Fixed about alignment

--- src/components/landingpage/AboutSection.tsx
+++ src/components/landingpage/AboutSection.tsx
@@ -1,12 +1,11 @@
-import Header from "./Header"
-
-
 const AboutSection = () => {
     return (
         <div className='w-full px-8 lg:px-[10vh] xl:px-[20vh] pt-[5rem] sm:pt-[16rem] md:pt-[39rem] lg:pt-[5rem] xl:pt-[8rem] 2xl:pt-[10rem] pb-6'>
             <div className='w-full grid grid-cols-1 md:grid-cols-2 gap-6'>
                 <div className="flex justify-center items-center">
-                    <Header title="About Us" backgroundColor="color:var(--primary-color)" textColor="white" />
+                    <div className={`w-full bg-[color:var(--primary-color)] flex justify-start items-center rounded-[15px] p-10`}>
+                        <h1 className={`text-white text-3xl font-[600] font-montserat`}>About Us</h1>
+                    </div>
                 </div>
                 <div className='w-full col-start-1 md:row-start-2 bg-[color:var(--secondary-color)] rounded-[15px] p-10'>
                     <p className=''>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
