181bb5e44d516cadfe8f7dbe89ffbc4ee7591a7b
Mac <mac@Macs-MacBook-Pro.local>
2024-01-12 20:16:49 +0530

    Navbar and Sidebar text size adjustments

--- src/components/global-components/Sidebar.tsx
+++ src/components/global-components/Sidebar.tsx
@@ -9,7 +9,7 @@ const Sidebar = () => {
   const [sidebarToggle, setSidebarToggle] = useRecoilState<boolean>(sidebarToggleAtom)
 
   return (
-    <div className={`block md:hidden translate-x-1 bg-[color:var(--primary-color)] fixed right-0 z-[100] transition-all duration-150 rounded-l-[15px] ease-in-out ${sidebarToggle ? 'w-[80vw] h-screen' : 'w-0 h-0 overflow-hidden'}`}>
+    <div className={`block md:hidden translate-x-1 bg-black fixed right-0 z-[100] transition-all duration-150 rounded-l-[15px] ease-in-out ${sidebarToggle ? 'w-[80vw] h-screen' : 'w-0 h-0 overflow-hidden'}`}>
       <div className="w-full flex justify-start px-6 mt-8">
         <img src={cross} className="w-[40px]" onClick={() => setSidebarToggle(false)} alt="" />
       </div>
@@ -21,9 +21,9 @@ const Sidebar = () => {
         </div>
       </div>
       <div className="flex flex-col justify-center items-center mt-[80px] gap-6">
-        <Link onClick={() => setSidebarToggle(false)} to='/case-studies'><h1 className="text-[30px] font-[500] text-gray-200">Case Studies</h1></Link>
-        <Link onClick={() => setSidebarToggle(false)} to='/about'><h1 className="text-[30px] font-[500] text-gray-200">About Us</h1></Link>
-        <Link onClick={() => setSidebarToggle(false)} to='/contact'><h1 className="text-[30px] font-[500] text-gray-200">Contact Us</h1></Link>
+        <Link onClick={() => setSidebarToggle(false)} to='/case-studies'><h1 className="text-[25px] font-[500] text-gray-200">Case Studies</h1></Link>
+        <Link onClick={() => setSidebarToggle(false)} to='/about'><h1 className="text-[25px] font-[500] text-gray-200">About Us</h1></Link>
+        <Link onClick={() => setSidebarToggle(false)} to='/contact'><h1 className="text-[25px] font-[500] text-gray-200">Contact Us</h1></Link>
       </div>
     </div>
   )
